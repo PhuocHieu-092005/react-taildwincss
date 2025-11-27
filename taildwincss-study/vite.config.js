df1df8bbfee8570a584ae78c2515ff8594e258e6
phuochieu13032006 <phanphuochieu1505@gmail.com>
2025-11-27 21:44:07 +0700

    Cấu hình deployment và update package.json

--- taildwincss-study/vite.config.js
+++ taildwincss-study/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Tên repository của bạn: react-tailwindcss
+const repoName = "react-tailwindcss";
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // THÊM THUỘC TÍNH BASE NÀY:
+  base: `/${repoName}/`,
   plugins: [react()],
-})
+});
